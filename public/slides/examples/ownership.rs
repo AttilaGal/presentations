74d3810f1413d5391aa7553ae5ce2edcf2d9c4c6
Attila Gal <attila.gal@digipolis.be>
2018-02-19 09:43:31 +0100

    added ownership example

--- public/slides/examples/ownership.rs
+++ public/slides/examples/ownership.rs
@@ -1,3 +1,14 @@
+fn printer(text: String) {
+    println!("Hello, front-end squad!");
+}
+
+fn main() {
+    let text = String::from("hello, front end squad!");
+    let text2 = text;
+    println!("{}", text);
+    println!("{}", text2);
+}
+
 fn main() {
     let text = String::from("hello, front end squad!");
     printer(text);
